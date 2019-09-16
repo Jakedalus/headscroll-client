47ea8157f8f2a5f5f32ffe739549048213f5fab8
Jakedalus <jacquedalus@gmail.com>
2019-09-16 14:08:29 -0400

    Add actions and reducers folders and files, setup store and rootReducer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '../store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
-function App() {
-  return (
-    <div className="App">
-      yo
-    </div>
-  );
+import Navbar from './Navbar';
+import Main from './Main';
+import { setAuthorizationToken, setCurrentUser } from '../store/actions/auth';
+
+const store = configureStore();
+
+if (localStorage.jwtToken) {
+  setAuthorizationToken(localStorage.jwtToken);
+  try {
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  } catch (err) {
+    store.dispatch(setCurrentUser({}));
+  }
 }
+
+const App = () => (
+  <Provider store={store} >
+    <Router>
+      <div className="App">
+        yo
+      </div>
+    </Router>
+  </Provider>
+
+);
+
 
 export default App;
