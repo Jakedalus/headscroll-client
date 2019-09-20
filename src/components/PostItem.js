e5a42289f918dd080832b620f3c100f22ffa0aca
Jakedalus <jacquedalus@gmail.com>
2019-09-20 13:58:25 -0400

    Start styling PostItems

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -6,21 +6,25 @@ import DefaultProfileImage from '../images/default-profile-image.png';
 const PostItem = ({ date, profileImageUrl, text, username, removePost, isCorrectUser }) => (
   <div>
     <li className="list-group-item">
-      <img 
-        src={profileImageUrl || DefaultProfileImage}
-        alt={username}
-        className="timeline-image"
-      />
-    
-      <div className="message-area">
+      <div className="post-heading">
+        <img 
+          src={profileImageUrl || DefaultProfileImage}
+          alt={username}
+          className="timeline-image"
+        />
         <Link to='/'>{username} &nbsp;</Link>
         <span className="text-muted">
           <Moment className="text-muted" format="Do MMM YYYY">
             {date}
           </Moment>
         </span>
-        <p>{text}</p>
         {isCorrectUser && <a onClick={removePost} className="btn btn-danger">Delete</a>}
+        {!isCorrectUser && <a onClick={removePost} className="btn btn-blank">      </a>}
+      </div>
+      
+      <div className="message-area">
+        <p>{text}</p>
+        
       </div>
     </li>
   </div>
