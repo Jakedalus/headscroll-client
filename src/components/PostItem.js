28e6de735a342bd1e1c6975249d94980d2f4ef78
Jakedalus <jacquedalus@gmail.com>
2019-10-29 15:32:56 -0400

    Add an input element for editing post, wire up editingPost flag

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,41 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
 import DefaultProfileImage from '../images/default-profile-image.png';
 import { editPost } from '../store/actions/posts';
 
-const PostItem = ({ createdAt, profileImageUrl, text, username, comments, editPost, removePost, isCorrectUser, user_id, post_id }) => (
-  <div>
-    <li className="list-group-item">
-      <div className="post-heading">
-        <img 
-          src={profileImageUrl || DefaultProfileImage}
-          alt={username}
-          className="timeline-image"
-        />
-        <Link to={`/users/${user_id}/profile`}>{username}</Link>
-        <span className="text-muted">
-        <Link to={`/users/${user_id}/posts/${post_id}`}>
-          <Moment className="text-muted" format="Do MMM YYYY">
-            {createdAt}
-          </Moment>
-        </Link>
-        </span>
-        {isCorrectUser && <a onClick={() => editPost({text: 'updates'})} className="btn btn-primary">Edit</a>}
-        {!isCorrectUser && <a className="btn btn-blank">      </a>}
-        {isCorrectUser && <a onClick={removePost} className="btn btn-danger">Delete</a>}
-        {!isCorrectUser && <a className="btn btn-blank">      </a>}
+class PostItem extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      post: this.props.text,
+      editingPost: false
+    };
+  }
+
+  onClickEditButton = () => {
+    this.setState({ editingPost: true });
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handgleEditPost = () => {
+    this.props.editPost({text: this.state.post});
+    this.setState({ editingPost: false });
+  };
+
+  render () {
+
+    let { createdAt, profileImageUrl, text, username, comments, removePost, isCorrectUser, user_id, post_id } = this.props;
+
+    return (
+      <div>
+        <li className="list-group-item">
+          <div className="post-heading">
+            <img 
+              src={profileImageUrl || DefaultProfileImage}
+              alt={username}
+              className="timeline-image"
+            />
+            <Link to={`/users/${user_id}/profile`}>{username}</Link>
+            <span className="text-muted">
+            <Link to={`/users/${user_id}/posts/${post_id}`}>
+              <Moment className="text-muted" format="Do MMM YYYY">
+                {createdAt}
+              </Moment>
+            </Link>
+            </span>
+            {isCorrectUser && <a onClick={this.onClickEditButton} className="btn btn-primary">Edit</a>}
+            {!isCorrectUser && <a className="btn btn-blank">      </a>}
+            {isCorrectUser && <a onClick={removePost} className="btn btn-danger">Delete</a>}
+            {!isCorrectUser && <a className="btn btn-blank">      </a>}
+          </div>
+          
+          <div className="message-area">
+            {
+              !this.state.editingPost 
+              && <p>{text}</p>
+            }
+            {
+              this.state.editingPost 
+              && 
+              <div>
+                <input 
+                  type="post" 
+                  name="post" 
+                  id="post" 
+                  value={this.state.post}
+                  onChange={this.handleChange}
+                />
+                <a onClick={this.handgleEditPost} className="btn btn-primary">Save</a>
+              </div>
+            }
+          </div>
+
+          <div className="post-footer">
+          {comments.length} Comments
+          </div>
+        </li>
       </div>
-      
-      <div className="message-area">
-        <p>{text}</p>
-        
-      </div>
-      <div className="post-footer">
-      {comments.length} Comments
-      </div>
-    </li>
-  </div>
-);
+    )
+  }
+}
 
 export default PostItem;
