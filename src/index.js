b97ac8dfdb8940dfc195309051b46f7ec4affb81
Jakedalus <jacquedalus@gmail.com>
2020-01-17 16:04:49 -0500

    Add meta-console.log debug

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
+console.log('ROOT index.js', process.env.NODE_ENV);
 if (process.env.NODE_ENV !== 'development') {
   console.log = () => {}
 }
+
+console.log('console.log:', console.log);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
