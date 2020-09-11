c022c42a814b14865f6d97de34cc134f8ce415ef
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-09-11 18:05:19 -0400

    Turn on console logging in production to fix post date error

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,47 +7,52 @@ import jwtDecode from 'jwt-decode';
 import Navbar from './Navbar';
 import Main from './Main';
 import Footer from '../components/Footer';
-import { setAuthorizationToken, setCurrentUser } from '../store/actions/auth';
+import {
+	setAuthorizationToken,
+	setCurrentUser
+} from '../store/actions/auth';
 
 // console.log('ROOT App.js', process.env.NODE_ENV);
-if (process.env.NODE_ENV !== 'development') {
-  console.log = () => {};
-}
+// if (process.env.NODE_ENV !== 'development') {
+//   console.log = () => {};
+// }
 
 console.log('console.log:', console.log);
 
 const store = configureStore();
 
 if (localStorage.jwtToken) {
-  setAuthorizationToken(localStorage.jwtToken);
-  try {
-    console.log('App.js, localStorage:', localStorage);
-    const decodedToken = jwtDecode(localStorage.jwtToken);
-    console.log('App.js, decodedToken:', decodedToken);
-    const profileImage = JSON.parse(localStorage.profileImage);
-    console.log('App.js, profileImage:', profileImage);
-    store.dispatch(setCurrentUser({
-      ...decodedToken, 
-      profileImage
-    }));
-  } catch (err) {
-    console.log('localStorage error!!', err);
-    store.dispatch(setCurrentUser({}));
-  }
+	setAuthorizationToken(localStorage.jwtToken);
+	try {
+		console.log('App.js, localStorage:', localStorage);
+		const decodedToken = jwtDecode(localStorage.jwtToken);
+		console.log('App.js, decodedToken:', decodedToken);
+		const profileImage = JSON.parse(
+			localStorage.profileImage
+		);
+		console.log('App.js, profileImage:', profileImage);
+		store.dispatch(
+			setCurrentUser({
+				...decodedToken,
+				profileImage
+			})
+		);
+	} catch (err) {
+		console.log('localStorage error!!', err);
+		store.dispatch(setCurrentUser({}));
+	}
 }
 
 const App = () => (
-  <Provider store={store} >
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Main />
-        <Footer />
-      </div>
-    </Router>
-  </Provider>
-
+	<Provider store={store}>
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<Main />
+				<Footer />
+			</div>
+		</Router>
+	</Provider>
 );
-
 
 export default App;
